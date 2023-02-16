6e99861320200b4b365f4effa6354d8509e996b8
unknown <davidkundrats@gmail.com>
2023-02-15 21:12:46 -0500

    latest version

--- python/src/xa_scraper.py
+++ python/src/xa_scraper.py
@@ -1,22 +1,19 @@
-from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 from pathlib import Path
 import pandas as pd 
 import numpy as np
 import requests
-import tkinter as tk
 import customtkinter as ui
-import time 
-import math
 import logging
 import ctypes
+from matplotlib import pyplot as plt
+global df 
 
 def run(): 
     linkInput = linkEntry.get() 
-    pathInput = pathEntry.get()
-    ebayScraper(linkInput, pathInput)
+    ebayScraper(linkInput)
 
-def ebayScraper(input, path): # scrapes user supplied ebay URL link for the first page and returns dataframe with scraped data
+def ebayScraper(input): # scrapes user supplied ebay URL link for the first page and returns dataframe with scraped data
 
      
     itemNames = [] # use to append item names and prices after scrape
@@ -24,7 +21,7 @@ def ebayScraper(input, path): # scrapes user supplied ebay URL link for the firs
     itemDate = []
     
     ebayUrl = input
-    path = path
+
     r = requests.request('GET', ebayUrl, headers ={'User-Agent' : 'Mozilla/5.0'})
     try:
         with requests.Session() as session: # open session 
@@ -43,41 +40,53 @@ def ebayScraper(input, path): # scrapes user supplied ebay URL link for the firs
                 priceTags.pop(0)
 
             else: 
-                return 'error occured with count of name and price tags: invalid link'  ## if elements aren't even something went wrong in counting the listings
+                raise Exception('error occured with count of name and price tags: invalid link')  ## if elements aren't even something went wrong in counting the listings
         
             if(len(nameTags) == len(priceTags) == len(itemDate)): # verify lengths are the same otherwise exit with status code
                 for i in range(0, len(nameTags)): # all same length doesnt matter which is used for loop 
                     itemNames.append(nameTags[i].get_text())
                     itemPrices.append(priceTags[i].get_text())
-                createDf(itemNames, itemPrices, itemDate, path + '/out.csv')
+                createDf(itemNames, itemPrices, itemDate)
        
             else: 
-                return "item count + price count and date count mismatch - unable to tabulate data", -1 #if they arent the same its issue with the date scrape
+                raise Exception('item count + price count and date count mismatch - unable to tabulate data') #if they arent the same its issue with the date scrape
 
     except Exception as Argument: 
         logging.exception(Argument)
 
-def createDf(name, price, date, path):  # create dataframe with the data
-    path = Path(path)       
+def createDf(name, price, date):  # create dataframe with the data
+    global df 
     df = pd.DataFrame({'Listed Name': name, 'Sold Price': price, 'Date Sold': date})
+
+    df['Date Sold'] = df['Date Sold'].str.replace('Sold  ?' , '') #clean data 
     columns = df.columns
-    df['Date Sold'] = df['Date Sold'].str.replace('Sold  ?' , '')
     df[columns] = df[columns].replace({'\$':''}, regex= True)
+    df['Sold Price'] = df['Sold Price'].astype(float) # cast prices as ints as they were scraped as strings
     df.set_index(['Date Sold'], inplace= True)
-    path.parent.mkdir(parents = True, exist_ok = True)
-    df.to_csv(path)
-    print(df)
-    displayComplete()
+
+    displayComplete() # route to next page
 
 def displayComplete(): 
     popup = ui.CTkToplevel(root)
     popup.geometry("400x150")
     popup.wm_title("Complete!")
-    button = ui.CTkButton(master = popup, text = "Visualize Data?", command = visualizeData)
-    button.pack(pady = 24, padx = 18)
+    label = ui.CTkLabel(master= popup, text= "Generated Dataframe. Process complete! Plot options below or save as CSV")
+    label.pack(pady =12, padx = 9)
+    pathEntry = ui.CTkEntry(master = popup, placeholder_text= "Set path to save csv")
+    pathEntry.pack(pady = 20, padx = 16)
+    plotButton = ui.CTkButton(master = popup, text = "Average Price", command = avgPrice)
+    plotButton.pack(pady = 24, padx = 18)
+    saveToCSVButton = ui.CTkButton(master= popup, text = "Save", command = saveAsCSV)
+    saveToCSVButton.pack(pady = 32, padx = 24)
+
+def saveAsCSV(): 
+    path = Path(path)
+    path.parent.mkdir(parents = True, exist_ok = True)
+    df.to_csv(path)
     
-def visualizeData(): 
-    pass
+def avgPrice(): 
+    plt.plot()
+    plt.show()
 
 ctypes.windll.shcore.SetProcessDpiAwareness(2)
 
@@ -98,9 +107,6 @@ label.pack(pady = 12, padx = 10)
 
 linkEntry = ui.CTkEntry(master = frame, placeholder_text = "Paste ebay link here")
 linkEntry.pack(pady = 12, padx = 10)
-
-pathEntry = ui.CTkEntry(master = frame, placeholder_text= "Set path to save csv")
-pathEntry.pack(pady = 20, padx = 16)
 
 button = ui.CTkButton(master = frame, text = "Search", command = run)
 button.pack(pady = 12, padx = 10)
