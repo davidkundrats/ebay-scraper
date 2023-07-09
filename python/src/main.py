dea12101cd1359cf0871e20862b7756f47ec3a6b
David Kundrats <davidkundrats@gmail.com>
2023-07-08 23:05:22 -0400

    outlined needed changs

--- python/src/main.py
+++ python/src/main.py
@@ -5,34 +5,32 @@ import pandas as pd
 import requests
 import numpy as np
 import mplcursors
-from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt\
 
-
-
+## TODO: fix the usage of this data frame variable from being global
 global DF
 
-
 def run():
-    """ Activated by search button within GUI. Captures user input 
+    """ Activated by search button. Captures user input 
       and calls ebay_scraper method with link_input as argument"""
     try: 
         link_input = input("Enter a sold listing url: ")
         ebay_scraper(link_input)
     except Exception as argument:
-        print(argument, file= "log.txt")
+        ## TODO: make this log error in txt file
         print("Your link is invalid. Please enter a valid link.")
         run()     
 
 
 def ebay_scraper(link_input):
     """Main algorithm of the program. Uses requests library, beautiful soup """
-    ebayLink = link_input
+    ebay_link = link_input
     item_names = []  
     item_prices = []
     item_date = []
     item_links = []
 
-    r = requests.request('GET', ebayLink, headers={
+    r = requests.request('GET', ebay_link, headers={
                          'User-Agent': 'Mozilla/5.0'}, timeout=60)
     try:
         soup = BeautifulSoup(r.content,  features='html.parser')
@@ -89,9 +87,6 @@ def ebay_scraper(link_input):
     except Exception as argument:
         print >> sys.stderr, argument ## TODO: make this more descriptive
         
-        
-
-
 def create_df(name, price, date):
     """Creates dataframe and truncates '$' and commas present. """
     global DF
@@ -108,16 +103,15 @@ def create_df(name, price, date):
     # cast prices as ints as they were scraped as strings
     DF['Sold Price'] = DF['Sold Price'].astype(float)
   
-
 def save_csv(path):
     """Method to save df as .csv using pathlib library"""
     path = Path(path)
     path.parent.mkdir(parents=True, exist_ok=True)
     DF.to_csv(path)
 
-
 def avg_price():
     """Method used to calculate and visualize the average sold price. """
+    ## TODO: fix functionality in displaying the item name on hover and lining up with the correct date
     DF['Date Sold'] = pd.to_datetime(DF['Date Sold'], format='%b %d %Y')
     ax = plt.gca()
     ax.plot(DF['Date Sold'], DF['Sold Price'], 'o', markersize=4) 
@@ -126,11 +120,14 @@ def avg_price():
     plt.show()
 
 def determine(inputs):
-   
+    """Method to determine which function to call based on user input"""
     if inputs == 'A': 
         avg_price()
     elif input == 'S': 
         path = input("Please enter a path to save data as CSV: ")
         save_csv(path)
+    else:
+        print("Invalid input. Please enter either 'A' or 'S' ")
+        determine(inputs)
 
 run()
