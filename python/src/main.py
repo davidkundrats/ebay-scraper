db1c27976f150d67b73b1f404ba00a7d4ffb6e26
davidkundrats <davidkundrats@gmail.com>
2023-10-10 17:20:19 -0400

    refactor user facing functions into utils.py and layout db implementation framework

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from matplotlib import pyplot as plt
-import sys
 from bs4 import BeautifulSoup
+from utils import save_csv
+from utils import avg_price
+import sys
 import pandas as pd
 import requests
 import mplcursors
@@ -98,42 +100,6 @@ def create_df(name, price, date):
     # cast prices as floats as they were scraped as strings
     df["Sold Price"] = df["Sold Price"].astype(float)
     return df
-
-
-def save_csv(df, path):
-    """Method to save a DataFrame as a .csv using pathlib library"""
-    path = Path(path)
-    path.parent.mkdir(parents=True, exist_ok=True)
-    df.to_csv(path)
-
-
-def avg_price(df):
-    """Method used to calculate and visualize the average sold price."""
-    df["Date Sold"] = pd.to_datetime(df["Date Sold"], format="%b %d %Y")
-    # Filter the DataFrame to keep only the last 30 days
-    last_30_days = df["Date Sold"].max() - pd.DateOffset(days=30)
-    filtered_df = df[df["Date Sold"] >= last_30_days]
-
-    fig, ax = plt.subplots()
-    ax.plot(filtered_df["Date Sold"], filtered_df["Sold Price"], "o", markersize=4)
-
-    median_price = filtered_df["Sold Price"].median()
-    ax.axhline(y=median_price, color="r", linestyle="--", label="Median Price")
-
-    ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter("%b %d"))
-
-    cursor = mplcursors.cursor(ax, hover=True)
-    cursor.connect(
-        "add",
-        lambda sel: sel.annotation.set_text(
-            filtered_df["Listed Name"].iloc[sel.target.index]
-        ),
-    )
-    ax.legend()
-    plt.xticks(rotation=45)
-    plt.tight_layout()
-    plt.show()
 
 
 def determine(df):
