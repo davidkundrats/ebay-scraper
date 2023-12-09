9dace2fd05c51f12ebcbb8ac11757a95af998c52
davidkundrats <davidkundrats@gmail.com>
2023-12-09 13:14:47 -0500

    implement decision tree regressor model

--- common/nlp_utils.py
+++ common/nlp_utils.py
@@ -1,7 +1,9 @@
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from nltk.tokenize import word_tokenize
 import pandas as pd
 import numpy as np
 ##streamline for testing purposes
@@ -19,14 +21,15 @@ def preprocess(df:pd.DataFrame)-> tuple:
     Returns:
     - tuple: A tuple containing X_train, X_test, y_train, y_test for model training and evaluation.
     """
-    tfidf_vectorizer = TfidfVectorizer()
-    X = tfidf_vectorizer.fit_transform(df['Listed Name'])
+    tokenized_data = [word_tokenize(sentance) for sentance in df['Listed Name']] 
+    vectorizer =  TfidfVectorizer()
+    X = vectorizer.fit_transform([" ".join(tokens) for tokens in tokenized_data])
     y = df['Sold Price']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size =.2, random_state = 42)
     return X_train, X_test, y_train, y_test
     
 
-def model_creation(*args)-> LinearRegression:
+def model_creation_lr(*args)-> LinearRegression:
     """
     Creates and trains a Linear Regression model using the provided training data and evaluates it
     on the test data. Prints the score of the model's performance.
@@ -46,7 +49,14 @@ def model_creation(*args)-> LinearRegression:
     print(report_score(model, X_test, y_test))
     return model
 
-def predict(model:LinearRegression,X_test, y_test ):
+def model_creation_dt(*args) -> DecisionTreeRegressor:
+    X_train, X_test, y_train, y_test = args
+    model = DecisionTreeRegressor()
+    model.fit(X_train, y_train)
+    print(report_score(model, X_test, y_test))
+    return model
+    
+def predict_lr(model:LinearRegression,X_test, y_test ):
     """
     Predicts 'Sold Price' using the provided trained Linear Regression model on test data.
 
@@ -61,6 +71,6 @@ def predict(model:LinearRegression,X_test, y_test ):
         print(f'Actual: {y_test[i]}')
         print(f'Predicted {model.predict(X_test[i])}')
 
-def report_score(model:LinearRegression, X_test, y_test):
+def report_score(model, X_test, y_test):
     return model.score(X_test, y_test)
     
